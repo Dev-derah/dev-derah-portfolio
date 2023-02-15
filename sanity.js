3f7838ad8543911e0f32c3b056c212ad0a9604e4
dev derah <chidera261@gmail.com>
2023-02-15 13:01:45 +0100

    sanity io

--- sanity.js
+++ sanity.js
@@ -3,9 +3,9 @@ import createImageUrlBuilder from '@sanity/image-url';
 
 export const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-  useCdn: true,
+  useCdn: process.env.NODE_ENV === 'production',
 };
 export const sanityClient = createClient(config);
 
--- utils/fetchSkill.js
+++ utils/fetchSkill.js
@@ -2,6 +2,5 @@ export const fetchSkills = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SANITY_BASE_URL}/api/getSkills`);
   const data = await res.json();
   const { skills } = data;
-  console.log('fetchingSkills', skills);
   return skills;
 };
