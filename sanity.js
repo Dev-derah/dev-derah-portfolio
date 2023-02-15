3b801189178d619f84d979feacdb09b676c3b183
dev derah <chidera261@gmail.com>
2023-02-15 22:08:15 +0100

    commit sanity configurations

--- sanity.js
+++ sanity.js
@@ -7,6 +7,6 @@ export const config = {
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: process.env.NODE_ENV === 'production',
 };
-export const sanityClient = createClient(config);
 
+export const sanityClient = createClient(config);
 export const urlFor = (source) => createImageUrlBuilder(config).image(source);
