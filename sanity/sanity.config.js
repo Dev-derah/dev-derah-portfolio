6fd7f5bdc5f81468a3f2064a9a88c002a9544a02
dev derah <chidera261@gmail.com>
2023-02-15 22:49:11 +0100

    sanity configuration changes

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -3,15 +3,12 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export default defineConfig({
   name: 'default',
   title: 'Portfolio',
 
-  projectId: projectId,
-  dataset: dataset,
-
+  projectId: 'oe20z6a7',
+  dataset: 'production',
   plugins: [deskTool(), visionTool()],
 
   schema: {
