82e9cd44dab02a656cd513f23877a07ffdc44c7c
dev derah <chidera261@gmail.com>
2023-02-18 05:44:29 +0100

    sanity url change

--- utils/fetchSkill.js
+++ utils/fetchSkill.js
@@ -1,5 +1,5 @@
 export const fetchSkills = async () => {
-  const res = await fetch('https://dev-derah.vercel.app/');
+  const res = await fetch('https://dev-derah.vercel.app/api/getSkills');
   const data = await res.json();
   const { skills } = data;
   return skills;
