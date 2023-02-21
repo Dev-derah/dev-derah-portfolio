3cdef029048649473597b1e222db27ae83ab4aac
dev derah <chidera261@gmail.com>
2023-02-21 14:58:12 +0100

    socials schema bug fixes

--- schemas/index.js
+++ schemas/index.js
@@ -2,4 +2,4 @@ import skill from './skill';
 import project from './project';
 import socials from './socials';
 
-export const schemaTypes = [project, skill,socials];
+export const schemaTypes = [project, skill, socials];
--- schemas/socials.js
+++ schemas/socials.js
@@ -1,8 +1,8 @@
 import { defineField, defineType } from 'sanity';
 
 export default defineType({
-  name: 'skill',
-  title: 'Skill',
+  name: 'socials',
+  title: 'Socials',
   type: 'document',
   fields: [
     defineField({
