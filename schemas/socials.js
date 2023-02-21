95d4f7381fb9e626d4877942f41105834c737843
dev derah <chidera261@gmail.com>
2023-02-21 15:05:14 +0100

    added more fields to schema

--- schemas/socials.js
+++ schemas/socials.js
@@ -6,12 +6,25 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
       name: 'link',
       title: 'Link',
       type: 'url',
       validation: (Rule) => Rule.uri({
-        scheme: ['http', 'https', 'mailto', 'tel'] }),
+        scheme: ['http', 'https', 'mailto', 'tel'],
+      }),
     }),
   ],
-
 });
