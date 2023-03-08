f1ed197304fb2dfdfb05d95110f18b997a27047b
dev derah <chidera261@gmail.com>
2023-03-08 17:47:48 +0100

    footer section changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,61 +2,78 @@
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import React from 'react';
 import { urlFor } from '../lib/sanity.client';
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
 
-const Footer = ({ socials }) => (
-  <motion.footer
-    variants={footerVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="footer-gradient" />
-    <div className={`${styles.innerWidth} flex flex-col gap-8 `}>
-      <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[40px] text-[25px] text-white">
-          Need a frontend enginner?
-        </h4>
-        <button
-          type="button"
-          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px] cursor-pointer"
-        >
-          <Image
-            src="/chat.svg"
-            alt="chat"
-            width={24}
-            height={24}
-            className="w-[24px] h-[24px] object-contain"
-          />
-          <span className="font-normal text-[16px] text-white">Lets talk</span>
-        </button>
-      </div>
-
-      <div className="flex flex-col">
-        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-
-        <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">Contact Me</h4>
-          <div className="flex gap-4">
-            {socials.map((social) => (
-              <a href={social.link} target="_blank" rel="noreferrer">
+const Footer = ({ socials }) => {
+  const filteredSocials = socials.filter(
+    (social) => social.name !== 'email',
+  );
+  const filteredemail = socials.filter(
+    (social) => social.name === 'email',
+  );
+  return (
+    <motion.footer
+      variants={footerVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      <div className="footer-gradient" />
+      <div className={`${styles.innerWidth} flex flex-col gap-8 `}>
+        {filteredemail.map((email) => (
+          <a href={email.link} target="_blank" rel="noreferrer">
+            <div className="flex items-center justify-between flex-wrap gap-5">
+              <h4 className="font-bold md:text-[40px] text-[25px] text-white">
+                Need a frontend enginner?
+              </h4>
+              <button
+                type="button"
+                className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px] cursor-pointer"
+              >
                 <Image
-                  key={social._id}
-                  height={24}
+                  src="/chat.svg"
+                  alt="chat"
                   width={24}
-                  src={urlFor(social.logo).url()}
-                  alt={social.name}
-                  className="w-[24px] h-[24px] object-contain cursor-pointer"
+                  height={24}
+                  className="w-[24px] h-[24px] object-contain"
                 />
-              </a>
-            ))}
+                <span className="font-normal text-[16px] text-white">
+                  Lets talk
+                </span>
+              </button>
+            </div>
+          </a>
+        ))}
+
+        <div className="flex flex-col">
+          <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+
+          <div className="flex items-center justify-between flex-wrap gap-4">
+            <h4 className="font-extrabold text-[24px] text-white">
+              Contact Me
+            </h4>
+            <div className="flex gap-4">
+              {filteredSocials.map((social) => (
+                <a href={social.link} target="_blank" rel="noreferrer">
+                  <Image
+                    key={social._id}
+                    height={24}
+                    width={24}
+                    src={urlFor(social.logo).url()}
+                    alt={social.name}
+                    className="w-[24px] h-[24px] object-contain cursor-pointer"
+                  />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </motion.footer>
-);
+    </motion.footer>
+  );
+};
 
 export default Footer;
