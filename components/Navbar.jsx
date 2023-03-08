3a57b7c6b5d2b1519ec292093d0e832fb7189a7b
dev derah <chidera261@gmail.com>
2023-03-08 20:30:35 +0100

    navbar changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,36 +3,47 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 
+import React, { useState } from 'react';
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
-const Navbar = () => (
-  <motion.nav
-    variants={navVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div
-      className={`${styles.innerWidth} mx-auto flex justify-between items-center gap-8`}
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} py-8 relative`}
     >
-      <Image
-        src="/my_logo.png"
-        height={64}
-        width={64}
-        className="h-16"
-        alt="Dev-Derah"
-      />
-      <Image
-        src="/menu.svg"
-        alt="menu"
-        height={16}
-        width={24}
-        className="w-[24px] h-16 object-contain cursor-pointer"
-      />
-    </div>
-  </motion.nav>
-);
+      <div className="absolute w-[50%] inset-0 gradient-01 z-50" />
+      <div
+        className={`${styles.innerWidth} mx-auto flex justify-between items-center gap-8`}
+      >
+        <Image
+          src="/my_logo.png"
+          height={64}
+          width={64}
+          className="h-16"
+          alt="Dev-Derah"
+        />
+        <Image
+          src="/menu.svg"
+          alt="menu"
+          height={16}
+          width={24}
+          className="w-[24px] h-16 object-contain cursor-pointer"
+          onClick={toggleMenu}
+        />
+      </div>
+      {menuOpen && (
+        <div className="h-screen w-screen backdrop-blur-2xl z-40 absolute top-0 left-0" />
+      )}
+    </motion.nav>
+  );
+};
 
 export default Navbar;
