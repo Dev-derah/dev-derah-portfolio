e6a1ec57ff29b138e9243b3439941ff37691d99c
dev derah <chidera261@gmail.com>
2023-03-26 17:13:22 +0100

    UI CHANGES

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
             animate={{ height: '100vh', opacity: 1 }}
             transition={{ duration: 0.3 }}
             exit="exit"
-            className="nav-container text-white h-4/5 w-[70%] backdrop-blur-[80px] z-40 absolute top-0 right-0 overflow-hidden md:w-[30%] "
+            className="nav-container text-white h-4/5 w-[70%] backdrop-blur-[80px] z-40 absolute top-0 right-0 overflow-hidden md:w-[25%] "
           >
             <nav className="flex justify-center items-end h-full w-full flex-col">
               <motion.a
